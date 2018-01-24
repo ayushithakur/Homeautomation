00e130f954056160449ffefa5d85a504a1c93f55
Abhisarchoudhary <35758233+Abhisarchoudhary@users.noreply.github.com>
2018-01-24 12:29:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+$sock = socket_create(AF_INET, SOCK_STREAM, 0);
+if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+ 
+echo "Socket created \n </br>";
+ 
+if(!socket_connect($sock , '216.58.196.4' , 80))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not connect: [$errorcode] $errormsg \n");
+}
+ 
+echo "Connection established \n </br>";
+ 
+$message = "GET / HTTP/1.0\r\n\r\n";
+ 
+//Send the message to the server
+if( ! socket_send ( $sock , $message , strlen($message) , 0))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not send data: [$errorcode] $errormsg \n");
+}
+ 
+echo "Message send successfully \n </br>";
+//Now receive reply from server
+if(socket_recv ( $sock , $buf , 2045 , MSG_WAITALL ) === FALSE)
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not receive data: [$errorcode] $errormsg \n");
+}
+ 
+//print the received message
+echo $buf."</br>";
+socket_close($sock);
+?>
